1133b6818385320e800d2ee7913a98c11bae95b4
0yukali0 <a08h0283@gmail.com>
2021-02-18 10:06:19 +0800

    my-scheduler

---
+++ fiternode.go
@@ -0,0 +1,8 @@
+package core
+
+import v1 "kubernetes/staging/src/k8s.io/api/core/v1"
+
+func (g *genericScheduler) PubToPredict(pod *v1.Pod, nodes *[]v1.Node) *[]v1.Node {
+	hosts, _, _ := g.findNodesThatFit(pod, nodes)
+	return hosts
+}
